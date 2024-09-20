e551682d40e06fa7884d041debc28c97a9451d80
cronobacter <antoniorafaelariasromero@gmail.com>
2024-09-19 22:19:44 -0500

    Add global constants for the project

---
+++ src/ref_command.rs
@@ -0,0 +1,16 @@
+
+pub const NAME: &str = "ld";
+pub const VERSION: &str = " v0.1";
+pub const HELP: &str = r#"Usage:
+    fac <option> <directory>
+
+    Options:
+        [--help, -h]
+            print help
+        [--version, -v]
+            print command version
+        [--list -l]
+            detailed list format
+        [--all, -a]
+            include hidden files
+"#;
