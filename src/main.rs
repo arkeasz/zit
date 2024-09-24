c7c97eafb7651b8d3225e75dbb3ebd7d9e1914d4
cronobacter <antoniorafaelariasromero@gmail.com>
2024-09-23 22:42:14 -0500

    Refactor main display logic to support list and non-list modes
    - Added conditional logic to display entries in list format (`--list` flag) or in a simplified format.
    - If the `--list` flag is provided, entries are displayed with detailed information such as mode, last modified date, and length.
    - Otherwise, entries are displayed in a single-line format for a more compact view.

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,24 @@ pub mod ref_command;
 pub mod parser;
 use std::{env, fmt::format};
 use app::App;
+use parser::Option;
 use ref_command::*;
 
 
 fn main() -> std::io::Result<()> {
     let app: App = App::init();
 
-
     for dir in &app.dirs {
-        println!("{}", dir);
         for entry in &app.entries    {
             if dir == &entry.father  {
-                println!(
-                    "{:<6} \t {:<19} {:>8} {}",
-                    entry.mode, entry.last_modified, entry.lenght, entry.name
-                );
+                if app.options.contains(&Option::List)  {
+                    println!(
+                        "{:<6} \t {:<19} {:>8} {}",
+                        entry.mode, entry.last_modified, entry.lenght, entry.name
+                    );
+                } else {
+                    print!("{} \t", entry.name)
+                }
             }
 
         }
